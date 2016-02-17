82d6f51ee2555427b1b7c9b312bda4c7edd9e14a
Mek Karpeles <michael.karpeles@gmail.com>
2016-02-17 12:35:09 -0800

    basic UI for iiif search

---
+++ views/endpoints.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+endpoints.py
+~~~~~~~~~~~~
+
+:copyright: (c) 2016 by Anonymous.
+:license: see LICENSE for more details.
+"""
+
+from flask import render_template
+from flask.views import MethodView
+
+
+urls = ()
